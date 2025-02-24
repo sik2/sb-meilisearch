2eed54a6781a40476ae99aaa21309e3b1475d486
sik2 <bickwang123@gmail.com>
2025-02-24 11:49:04 +0900

    init

---
+++ src/main/java/com/ll/meilisearch/MeilisearchApplication.java
@@ -0,0 +1,13 @@
+package com.ll.meilisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeilisearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeilisearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/meilisearch/MeilisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.meilisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeilisearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
